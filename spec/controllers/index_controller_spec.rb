eb1eb69d6651e3ca71d00d31b011bae22c7d1fb3
rasnom <ransom.byers@gmail.com>
2017-09-05 07:05:28 +0000

    Add index view as site root

--- spec/controllers/index_controller_spec.rb
+++ spec/controllers/index_controller_spec.rb
@@ -2,4 +2,13 @@ require 'rails_helper'
 
 RSpec.describe IndexController, type: :controller do
 
+  describe 'GET Index' do
+    it 'renders the index template' do
+      get :index
+      expect(response).to render_template('index')
+    end
+  end
+
+
+
 end
