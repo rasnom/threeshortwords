81bb92f2e453f822992214eaf5bde63fc7c29393
Ransom Byers <ransom.byers@gmail.com>
2018-10-02 19:42:47 -0700

    Comment out big_hug_thesaurus model and spec since it is not currently being used

--- spec/services/big_huge_thesaurus_spec.rb
+++ spec/services/big_huge_thesaurus_spec.rb
@@ -1,29 +1,29 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe 'BigHugeThesaurus' do
-  let(:thesaurus) { BigHugeThesaurus.new }
+# RSpec.describe 'BigHugeThesaurus' do
+#   let(:thesaurus) { BigHugeThesaurus.new }
 
-  it 'can create a thesaurus service with a name' do
-    expect(thesaurus.name).to eq 'Big Huge Thesaurus'
-  end
+#   it 'can create a thesaurus service with a name' do
+#     expect(thesaurus.name).to eq 'Big Huge Thesaurus'
+#   end
 
-  it 'has an api key' do
-    expect(thesaurus.api_key).to be_a(String)
-  end
+#   it 'has an api key' do
+#     expect(thesaurus.api_key).to be_a(String)
+#   end
 
-  describe 'synonyms' do
-    it 'returns a list of synonyms' do
-      allow(HTTParty).to receive(:get).and_return('{"noun":{"syn":["this","and","that"]}}')
+#   describe 'synonyms' do
+#     it 'returns a list of synonyms' do
+#       allow(HTTParty).to receive(:get).and_return('{"noun":{"syn":["this","and","that"]}}')
 
-      result = thesaurus.synonyms('anything')
-      expect(result).to be_a(Array)
-      expect(result.first).to be_a(String)
-    end
+#       result = thesaurus.synonyms('anything')
+#       expect(result).to be_a(Array)
+#       expect(result.first).to be_a(String)
+#     end
 
-    it 'returns an empty array if no word is specified' do
-      result = thesaurus.synonyms('')
-      expect(result).to eq [] 
-    end
-  end
+#     it 'returns an empty array if no word is specified' do
+#       result = thesaurus.synonyms('')
+#       expect(result).to eq [] 
+#     end
+#   end
 
-end
+# end
